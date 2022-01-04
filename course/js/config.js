2c38fa2b2042f7e708f2ebfa37cd8de6e2c1cd7b
JuLinux <julia.ingridsantos.7@gmail.com>
2022-01-04 20:33:49 -0300

    first commit

---
+++ course/js/config.js
@@ -0,0 +1,10 @@
+
+function setConfig(){
+    var texts = {
+        "title":"Hidrofisio"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
