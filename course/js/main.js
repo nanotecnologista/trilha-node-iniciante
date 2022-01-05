74bc26249dcb5acd775e6ed5f4c72e47a795db43
JuLinux <julia.ingridsantos.7@gmail.com>
2022-01-04 22:11:51 -0300

    Adicionando novos registros

--- course/js/main.js
+++ course/js/main.js
@@ -33,5 +33,13 @@ function formatValue(value){
     return str
 }
 
+function addData() {
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+    
+    list.unshift({"desc":desc , "amount":amount , "value": value});
+    setList(list);
+}
 setList(list)
 console.log(getTotal(list));
