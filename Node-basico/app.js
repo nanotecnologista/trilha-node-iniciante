41081b907c9d940c504a6b5dada6acb6e5bfe3d4
Juh Linux <julia.ingridsantos.7@gmail.com>
2022-01-12 20:05:26 -0300

    TypeError: request.writeHead is not a function

--- Node-basico/app.js
+++ Node-basico/app.js
@@ -1,5 +1,29 @@
-console.log ('Hello World')
+/* console.log ('Hello World')
 var a = 2 , b = '2'
 var c = a+b;
 
-console.log('c ->',c)
+console.log('c ->',c) */
+
+var http = require('http')
+
+function handle(request, response){
+    request.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+
+    response.write ('<!DOCTYPE "html">')
+    response.write ('<html>')
+    response.write ('<head><title> http Module</title></head>')
+    response.write ('<body>')
+    response.write ('<h1>Olá mundo</h1>')
+    response.write ('</body>')
+    response.write ('</html>')
+    response.end ()
+
+}
+
+var server = http.createServer(handle)
+
+server.listen(3000, function () {
+    console.log ('Server is listening at port 3000')
+})
