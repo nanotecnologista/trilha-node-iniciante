23b8c06c9a517494cfd62bd8512bc17b573f465e
Juh Linux <julia.ingridsantos.7@gmail.com>
2022-01-21 16:36:00 -0300

    Reconfigurando a tabela

--- Autenticação com Node e Passport/auth_node_passport/src/controller/users/remove.js
+++ Autenticação com Node e Passport/auth_node_passport/src/controller/users/remove.js
@@ -1,5 +1,5 @@
 const User = require('./../../model/user')
 
 module.exports = (req, res) =>{
-    Userz.findByIdAndRemove(req.params.id).then(()=> res.redirect('/users')).catch(error => console.log(error) )
+    User.findByIdAndRemove(req.params.id).then(()=> res.redirect('/users')).catch(error => console.log(error) )
 }
